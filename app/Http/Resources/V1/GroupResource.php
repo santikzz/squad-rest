c4ef2e8063c7cebe29349b0b8b53b3d382151213
ssanti86 <monsterspeed02@gmail.com>
2024-03-17 12:45:52 -0300

    group isMember and hasJoinRequest added to resource

--- app/Http/Resources/V1/GroupResource.php
+++ app/Http/Resources/V1/GroupResource.php
@@ -4,25 +4,39 @@ namespace App\Http\Resources\V1;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\DB;
 
 class GroupResource extends JsonResource
 {
     // get group data
     public function toArray(Request $request): array
     {
+
+        $user = $request->user();
+
+        $joined = $this->members->contains($user);
+
+        $joinRequest = DB::table('user_group_join_request')
+            ->where('user_id', $user->id)
+            ->where('group_id', $this->id)
+            ->exists();
+
+        // $joinRequest = $joinRequestExists ? 'pending' : 'none';
+
         return [
             'ulid' => $this->ulid,
             'owner' => new UserGroupResource($this->owner),
             'title' => $this->title,
             'description' => $this->description,
-            'tags' => $this->tags->pluck('tag'), //GroupTagResource::collection($this->tags),
+            'tags' => $this->tags->pluck('tag'),
             'facultad' => $this->carrera->facultad->name,
             'carrera' => $this->carrera->name,
             'maxMembers' => $this->max_members,
-            'membersCount' => ($this->members->count()+1),
+            'membersCount' => ($this->members->count() + 1),
             'privacy' => $this->privacy,
             'members' => UserGroupResource::collection($this->members),
             'creationDate' => $this->created_at,
+            'user' => ['isMember' => $joined, 'hasJoinRequest' => $joinRequest]
         ];
     }
 }
